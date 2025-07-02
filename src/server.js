83d9c99eb41d6a45d4e873445167b3c30faa7408
FikriBaihaqi73 <fikrimuhammad38993@gmail.com>
2025-07-02 13:20:35 +0700

    fix: fix bug eror

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-import 'pg'; // Explicitly import pg for Vercel deployment issues
 import express from 'express';
 import todoRoutes from './routes/todos.routes.js';
 import cors from 'cors';
@@ -6,7 +5,6 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import swaggerUiDist from 'swagger-ui-dist'; // Menonaktifkan ini karena tidak diperlukan dengan swaggerUi.serve
 
 // Untuk ESM, __dirname tidak tersedia, jadi kita mendapatkannya
 const __filename = fileURLToPath(import.meta.url);
@@ -20,13 +18,15 @@ app.use(express.static('public'));
 
 // Muat file spesifikasi Swagger YAML
 const swaggerSpec = YAML.load(path.join(process.cwd(), 'src/docs/swagger.yaml'));
+
+// Konfigurasi CORS
 app.use(cors({
   origin: '*', // Mengizinkan dari semua origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Mengizinkan semua metode yang digunakan
   preflightContinue: false,
   optionsSuccessStatus: 204
 }));
-app.options('*', cors()); // Mengatasi preflight OPTIONS request
+// app.options('*', cors()); // Mengatasi preflight OPTIONS request (dihapus karena sering menyebabkan masalah)
 app.use(express.json());
 
 app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
@@ -37,5 +37,4 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    // console.log(`Swagger UI static files served from: ${swaggerUiDist.getAbsoluteFSPath()}`); // Baris ini tidak lagi relevan
 });
